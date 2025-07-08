08c74b225a9f852d6b73dd607dcdb28b6db6e0fa
mvsairavikiran25 <mvsairavikiran25@gmail.com>
2025-07-08 14:09:01 +0530

    Add files via upload

---
+++ task1_mathematical_operations.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
+

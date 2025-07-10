4b83106f3205134ea10b16df7cf8f1bc632be3a5
mvsairavikiran25 <mvsairavikiran25@gmail.com>
2025-07-10 21:53:37 +0530

    Add files via upload

---
+++ tast 2 Using the Math Module for Calculations.py
@@ -0,0 +1,11 @@
+user_input = int(input("Enter a Number:"))
+
+import math
+result = math.sqrt(user_input)
+print("Square root:", result)
+
+result = math.log10(user_input)
+print("Logarithm:", result)
+
+result = math.sin(user_input)
+print("Sine:", result)
